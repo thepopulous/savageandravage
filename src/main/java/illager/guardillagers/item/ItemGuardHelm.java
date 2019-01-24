83440ca44721405a9fcb29decaa63ba4dc8bdd9f
pentantan <shutux500@gmail.com>
2019-01-24 22:47:04 +0900

    add helm overlay!

--- src/main/java/illager/guardillagers/item/ItemGuardHelm.java
+++ src/main/java/illager/guardillagers/item/ItemGuardHelm.java
@@ -2,17 +2,26 @@ package illager.guardillagers.item;
 
 import illager.guardillagers.GuardIllagers;
 import illager.guardillagers.client.model.ModelGuardHat;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.model.ModelBiped;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class ItemGuardHelm extends ItemArmor {
+    protected static final ResourceLocation HAT_TEX = new ResourceLocation(GuardIllagers.MODID, "textures/overlay/guard_perspective.png");
+
     public ItemGuardHelm(IArmorMaterial materialIn, EntityEquipmentSlot slots, Builder builder) {
         super(materialIn, slots, builder);
     }
@@ -27,4 +36,26 @@ public class ItemGuardHelm extends ItemArmor {
         return GuardIllagers.MODID + ":" + "textures/armor/guard_hat.png";
     }
 
+    @Override
+    public void renderHelmetOverlay(ItemStack stack, EntityPlayer player, int width, int height, float partialTicks) {
+
+        GlStateManager.disableDepthTest();
+        GlStateManager.depthMask(false);
+        GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GlStateManager.disableAlphaTest();
+        Minecraft.getInstance().getTextureManager().bindTexture(HAT_TEX);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(0.0D, (double) height, -90.0D).tex(0.0D, 1.0D).endVertex();
+        bufferbuilder.pos((double) width, (double) height, -90.0D).tex(1.0D, 1.0D).endVertex();
+        bufferbuilder.pos((double) width, 0.0D, -90.0D).tex(1.0D, 0.0D).endVertex();
+        bufferbuilder.pos(0.0D, 0.0D, -90.0D).tex(0.0D, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.depthMask(true);
+        GlStateManager.enableDepthTest();
+        GlStateManager.enableAlphaTest();
+        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+    }
 }
