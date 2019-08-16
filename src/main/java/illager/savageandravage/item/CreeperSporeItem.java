c6dc30ada40620621630948ff072dad660997264
pentantan <shutux500@gmail.com>
2019-08-16 16:00:26 +0900

    creeperSpore can only use on block

--- src/main/java/illager/savageandravage/item/CreeperSporeItem.java
+++ src/main/java/illager/savageandravage/item/CreeperSporeItem.java
@@ -1,11 +1,18 @@
 package illager.savageandravage.item;
 
-import illager.savageandravage.entity.projectile.CreeperSporeEntity;
+import illager.savageandravage.entity.projectile.SporeCloudEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class CreeperSporeItem extends Item {
@@ -19,19 +26,43 @@ public class CreeperSporeItem extends Item {
      */
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack itemstack = playerIn.getHeldItem(handIn);
-        if (!playerIn.abilities.isCreativeMode) {
-            itemstack.shrink(1);
-        }
 
-        worldIn.playSound((PlayerEntity) null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_EGG_THROW, SoundCategory.PLAYERS, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
-        if (!worldIn.isRemote) {
-            CreeperSporeEntity sporeentity = new CreeperSporeEntity(worldIn, playerIn);
-            sporeentity.func_213884_b(itemstack);
-            sporeentity.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
-            worldIn.addEntity(sporeentity);
-        }
+        RayTraceResult raytraceresult = rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.NONE);
+        if (raytraceresult.getType() == RayTraceResult.Type.MISS) {
+            return new ActionResult<>(ActionResultType.PASS, itemstack);
+        } else if (raytraceresult.getType() != RayTraceResult.Type.BLOCK) {
+            return new ActionResult<>(ActionResultType.PASS, itemstack);
+        } else {
+            if (!playerIn.abilities.isCreativeMode) {
+                itemstack.shrink(1);
+            }
 
-        playerIn.addStat(Stats.ITEM_USED.get(this));
-        return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
+
+            playerIn.setActiveHand(handIn);
+            BlockRayTraceResult blockraytraceresult = (BlockRayTraceResult) raytraceresult;
+            BlockPos blockpos = blockraytraceresult.getPos();
+            if (worldIn.isBlockModifiable(playerIn, blockpos) && playerIn.canPlayerEdit(blockpos, blockraytraceresult.getFace(), itemstack)) {
+                worldIn.playSound((PlayerEntity) null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_EGG_THROW, SoundCategory.PLAYERS, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
+
+
+                if (!worldIn.isRemote) {
+
+                    SporeCloudEntity areaeffectcloudentity = new SporeCloudEntity(worldIn, blockpos.getX(), blockpos.getY() + 1.0F, blockpos.getZ());
+                    areaeffectcloudentity.setOwner(playerIn);
+                    areaeffectcloudentity.setParticleData(ParticleTypes.SNEEZE);
+                    areaeffectcloudentity.setRadius(2.0F);
+                    areaeffectcloudentity.setRadiusOnUse(-0.05F);
+                    areaeffectcloudentity.setDuration(200);
+                    areaeffectcloudentity.setRadiusPerTick(-areaeffectcloudentity.getRadius() / (float) areaeffectcloudentity.getDuration());
+                    areaeffectcloudentity.addEffect(new EffectInstance(Effects.NAUSEA, 100, 0));
+
+
+                    worldIn.addEntity(areaeffectcloudentity);
+
+                }
+            }
+            playerIn.addStat(Stats.ITEM_USED.get(this));
+            return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
+        }
     }
 }
