4114c77da2773f101084340bb18f5766d907f80d
pentantan <shutux500@gmail.com>
2019-09-22 06:15:00 +0900

    Change Armor Points to Armor Toughness for Tenacity effect

--- src/main/java/illager/savageandravage/init/SavageEffectRegistry.java
+++ src/main/java/illager/savageandravage/init/SavageEffectRegistry.java
@@ -14,7 +14,7 @@ import static illager.savageandravage.SavageAndRavageCore.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class SavageEffectRegistry {
-    public static final Effect TENACITY = new TenacityEffect(EffectType.BENEFICIAL, 0xe92f2f).addAttributesModifier(SharedMonsterAttributes.ATTACK_DAMAGE, "648D7064-6A60-4F59-8ABE-C2C23A6DD7A9", 0.0D, AttributeModifier.Operation.ADDITION).addAttributesModifier(SharedMonsterAttributes.ARMOR, "7E0292F2-9434-48D5-A29F-9583AF7DF27F", 0.0D, AttributeModifier.Operation.ADDITION);
+    public static final Effect TENACITY = new TenacityEffect(EffectType.BENEFICIAL, 0xe92f2f).addAttributesModifier(SharedMonsterAttributes.ATTACK_DAMAGE, "648D7064-6A60-4F59-8ABE-C2C23A6DD7A9", 0.0D, AttributeModifier.Operation.ADDITION).addAttributesModifier(SharedMonsterAttributes.ARMOR_TOUGHNESS, "7E0292F2-9434-48D5-A29F-9583AF7DF27F", 0.0D, AttributeModifier.Operation.ADDITION);
     public static final Effect BEAST = new BeastBrewEffect(EffectType.NEUTRAL, 0x84199);
 
     @SubscribeEvent
