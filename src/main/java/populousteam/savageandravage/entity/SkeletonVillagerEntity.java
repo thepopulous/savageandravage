fb6eb0a117017d3a1d5155d888a56d15ddea7f3c
pentantan <shutux500@gmail.com>
2019-11-02 17:26:32 +0900

    remove unuse loottable method

--- src/main/java/populousteam/savageandravage/entity/SkeletonVillagerEntity.java
+++ src/main/java/populousteam/savageandravage/entity/SkeletonVillagerEntity.java
@@ -15,7 +15,6 @@ import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.MathHelper;
@@ -26,7 +25,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import populousteam.savageandravage.api.entity.EntityArmPose;
 import populousteam.savageandravage.api.entity.IEntityArm;
-import populousteam.savageandravage.init.SavageLootTables;
 
 public class SkeletonVillagerEntity extends AbstractSkeletonEntity implements ICrossbowUser, IEntityArm {
     private final RangedBowAttackGoal<SkeletonVillagerEntity> aiArrowAttack = new RangedBowAttackGoal<>(this, 1.0D, 20, 16.0F);
@@ -125,11 +123,6 @@ public class SkeletonVillagerEntity extends AbstractSkeletonEntity implements IC
         Vector3f vector3f = this.func_213673_a(new Vec3d(d0, d3, d1), p_213670_4_);
         p_213670_3_.shoot((double) vector3f.getX(), (double) vector3f.getY(), (double) vector3f.getZ(), 1.6F, (float) (14 - this.world.getDifficulty().getId() * 4));
         this.playSound(SoundEvents.ITEM_CROSSBOW_SHOOT, 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
-    }
-
-    @Override
-    protected ResourceLocation getLootTable() {
-        return SavageLootTables.SKELETON_VILLAGER;
     }
 
     private Vector3f func_213673_a(Vec3d p_213673_1_, float p_213673_2_) {
