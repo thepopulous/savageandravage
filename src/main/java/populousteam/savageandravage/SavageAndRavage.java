a4f363e82c83622ffa78cce179b5b14d027b45b9
Voliant <thatvaliantvole@gmail.com>
2019-11-10 17:49:12 +0000

    Added basic mod files and assets (committed to see if issue is with my workspace)

---
+++ src/main/java/populousteam/savageandravage/SavageAndRavage.java
@@ -0,0 +1,15 @@
+package populousteam.savageandravage;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = SavageAndRavage.MOD_ID)
+public class SavageAndRavage {
+
+    public static final String MOD_ID = "savageandravage";
+    public SavageAndRavage instance;
+
+    public SavageAndRavage(){
+
+    }
+
+}
